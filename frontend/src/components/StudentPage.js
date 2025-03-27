c100c44cb2d578e23efd648f5bcf39f0b6218b6c
Krishna Pahariya <krishna_p@cs.iitr.ac.in>
2025-03-27 17:17:01 +0530

    Update StudentPage.js

--- frontend/src/components/StudentPage.js
+++ frontend/src/components/StudentPage.js
@@ -1,16 +1,18 @@
 import { useState } from 'react';
+import { useParams } from 'react-router-dom';
 import Panel from './pages/Panel.js';
 import './styles/StudentPage.css';
 
 function StudentPage() {
     const [activePage, setActivePage] = useState("Home"); // Default active page
+    const { userId } = useParams();
 
     return (
         <div className="student-page">
             <Panel activePage={activePage} setActivePage={setActivePage} />
 
             <div className="content">
-                <h1>Welcome to Your {activePage} Page</h1>
+                <h1>Hello, {userId}</h1>
             </div>
         </div>
     );
