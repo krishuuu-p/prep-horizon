a382965bbd6377c7732a258a72e0432d588e59e4
Shorya-Panwar-IITR <shorya_p@cs.iitr.ac.in>
2025-04-02 15:51:17 +0530

    Minor issues resolved

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -45,7 +45,7 @@ function Login() {
     const handleLogin = async () => {
         try {
             const response = await axios.post('http://localhost:5000/login', {
-                id: userName,
+                id: user.userName,
                 password: password,
                 role: user.userType
             });
